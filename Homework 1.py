2f8671e257ac2c9b22f131ea5802615bb250901f
VovkTatyana <107269003+VovkTatyana@users.noreply.github.com>
2022-06-23 16:32:20 +0300

    Revert "Домашнее задание 3"

--- Homework 1.py
+++
@@ -1,8 +0,0 @@
-def my_f(num_1, num_2):
-    try:
-        return num_1//num_2
-    except ZeroDivisionError:
-        print('На ноль делить нельзя!')
-
-print(my_f(int(input('Введите число которое хотите разделить: ')),int(input('Введите число на которое хотите разделить: '))))
-
